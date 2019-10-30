674d3f02d09734cc0b6cbb841fefcceab182e20b
Genevieve Labbe <17420847+glabbe@users.noreply.github.com>
2019-10-30 12:50:15 -0400

    Delete create_reads.sh

--- 8. Methods/8.4 Benchmarking the BioHansel QA-QC module/8.4.2 Generation of artificially contaminated datasets/create_reads.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for fasta in *.fasta
-do
-    sbatch -p Reference/Surveillance --mem=12G -c 4 --wrap="art_illumina -ss MSv3  -f 500 -i "$fasta" -l 250 -m 800 -s 42 -p -o "$fasta"_"
-    printf "$fasta \n"
-done
-
-echo "done :)"
